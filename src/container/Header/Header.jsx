973047b3b26157a7656c05429eef78e90be67c23
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-24 09:24:22 -0300

    starting implementation of 1st header section -- will continue later

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+      {/*
+      - - imagine the header horizontally, on the left side we have the information: title + description, on the right a nice image
+      */}
+      <div className='app__wrapper_info'>
+        <SubHeading  title=''/>
+        <h1 className='app__header-h1'>They Key to Fine Dining</h1>
+      </div>
+
+      <div className='app__wrapper_img'>
+       </div>
   </div>
 );
 
