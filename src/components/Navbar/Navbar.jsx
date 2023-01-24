9b0ecb9931e85d4d950b44541c239178426ad091
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-23 21:49:25 -0300

    finished working on hamburger menu jsx file - gotta complete css now

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -25,7 +25,19 @@ const Navbar = () => (
       <a href='/' className='p__opensans'>Book a Table</a> {/*use slash / to be directed to home*/}
     </div>
     <div className='app__navbar-smallscreen'>
-      <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => {}}/>
+      {/*implementing hamburger menu for small screens*/}     
+     <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => {}}/>
+      <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+        <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => {}}/>
+        <ul className='app__navbar-smallscreen-links'>
+          <li className='p__opensans'><a href='#home'>Home</a></li>
+          <li className='p__opensans'><a href='#about'>About</a></li>
+          <li className='p__opensans'><a href='#menu'>Menu</a></li>
+          <li className='p__opensans'><a href='#awards'>Awards</a></li>
+          <li className='p__opensans'><a href='#contact'>Contact</a></li>
+        </ul>
+      </div>
+     
     </div>
   </nav> 
 );
