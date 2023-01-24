812dd7daeb4d55870aa01407a991462d7ffe950b
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-23 21:59:49 -0300

    continuing work on navbar responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,41 +5,48 @@ import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
 import './Navbar.css';
 
-const Navbar = () => (
-  <nav className= 'app__navbar'>
-    <div className='app__navbar-logo'> 
-      <img src={images.gericht} alt='app logo' />
-    </div>
-    <ul className='app__navbar-links'>
-      <li className='p__opensans'><a href='#home'>Home</a></li>
-      <li className='p__opensans'><a href='#about'>About</a></li>
-      <li className='p__opensans'><a href='#menu'>Menu</a></li>
-      <li className='p__opensans'><a href='#awards'>Awards</a></li>
-      <li className='p__opensans'><a href='#contact'>Contact</a></li>
-    </ul>
-    <div className='app__navbar-login'>
-      <a href='#login' className='p__opensans'>
-        Log in / Register
-      </a>
-      <div /> {/* a little div to make space */}
-      <a href='/' className='p__opensans'>Book a Table</a> {/*use slash / to be directed to home*/}
-    </div>
-    <div className='app__navbar-smallscreen'>
-      {/*implementing hamburger menu for small screens*/}     
-     <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => {}}/>
-      <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
-        <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => {}}/>
-        <ul className='app__navbar-smallscreen-links'>
-          <li className='p__opensans'><a href='#home'>Home</a></li>
-          <li className='p__opensans'><a href='#about'>About</a></li>
-          <li className='p__opensans'><a href='#menu'>Menu</a></li>
-          <li className='p__opensans'><a href='#awards'>Awards</a></li>
-          <li className='p__opensans'><a href='#contact'>Contact</a></li>
-        </ul>
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = React.useState(false);
+
+
+  return (
+    <nav className= 'app__navbar'>
+      <div className='app__navbar-logo'> 
+        <img src={images.gericht} alt='app logo' />
       </div>
-     
-    </div>
-  </nav> 
-);
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href='#home'>Home</a></li>
+        <li className='p__opensans'><a href='#about'>About</a></li>
+        <li className='p__opensans'><a href='#menu'>Menu</a></li>
+        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        <li className='p__opensans'><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className='app__navbar-login'>
+        <a href='#login' className='p__opensans'>
+          Log in / Register
+        </a>
+        <div /> {/* a little div to make space */}
+        <a href='/' className='p__opensans'>Book a Table</a> {/*use slash / to be directed to home*/}
+      </div>
+      <div className='app__navbar-smallscreen'>
+        {/*implementing hamburger menu for small screens*/}     
+      <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => setToggleMenu(true)}/>
+        {/* the trick below makes it so React only shows this if toggleMeny is set to TRUE */}
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)}/>
+            <ul className='app__navbar-smallscreen-links'>
+              <li className='p__opensans'><a href='#home'>Home</a></li>
+              <li className='p__opensans'><a href='#about'>About</a></li>
+              <li className='p__opensans'><a href='#menu'>Menu</a></li>
+              <li className='p__opensans'><a href='#awards'>Awards</a></li>
+              <li className='p__opensans'><a href='#contact'>Contact</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav> 
+  )
+};
 
 export default Navbar;
