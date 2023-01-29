97339444f78713a09b50ae791ad4a834dae490be
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-28 21:40:02 -0300

    completed intro , css file and now starting to work on the laurels section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import { images, data } from '../../constants';
+import { SubHeading } from '../../components';
+
 import './Laurels.css';
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id='awards'>
+    <div className='app__wrapper_info'> 
+      <SubHeading title="Awards & Recognition"/>
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+    </div> 
+    <div> 
+
+    </div>
   </div>
 );
 
