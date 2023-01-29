90ae1f8d05d4c3d254859caaff6c2a16a78144ba
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-29 09:33:18 -0300

    working on gallery section -- arrows to move right and left have been implemented

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import {images} from '../../constants'
+import {BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs'
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+
+
+const Gallery = () => {
+  return (
+    <div className='app__gallery flex__center' id='gallery'>
+       <div className='app__gallery-content'>
+          <SubHeading title="Instagram"/>
+          <h1 className='headtext__cormorant'>Photo Gallery</h1>
+          <p className='p__opensans' style={{color: "#AAA", marginTop: "2rem"}}>Lorem ipsum dolor sit amet Lorem ipsum dolor sit ametLorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+          <button type='button' className='custom__button'>View More</button>
+      </div> 
+
+      <div className='app__gallery-gallery'>
+       <img src={images.gallery03}/>
+      </div>
+   </div>
+  )
+};
 
 export default Gallery;
