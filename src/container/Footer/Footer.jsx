4bcba2ebb32203cd663961f0c1ac46db840f010a
Gabriel Chu Bottiglieri <gabrielchubotti@gmail.com>
2023-01-31 22:27:32 -0300

    Newsletter section looks OK for now. More to come tomorrow

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -5,7 +5,7 @@ import { FiFacebook, FiTwitter, FiInstagram} from 'react-icons/fi'
 import './Footer.css';
 
 const Footer = () => (
-  <div className='app__fotter section__padding'>
+  <div className='app__footer section__padding'>
     <FooterOverlay />
     <Newsletter />
   </div>
